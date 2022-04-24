a9743e24a6549b661cce05b5940907656c2e4cef
Daniel Kennett <92280740+dkong13@users.noreply.github.com>
2022-04-24 17:15:27 -0400

    Add files via upload

---
+++ Basic-Marketplace/BasicMarketplace/src/main/java/com/javaGroup/basicMarketplace/BasicMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.javaGroup.basicMarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicMarketplaceApplication.class, args);
+	}
+
+}
---
+++ Basic-Marketplace/BasicMarketplace/src/main/java/com/javaGroup/basicMarketplace/models/Product.java
@@ -0,0 +1,5 @@
+package com.javaGroup.basicMarketplace.models;
+
+public class Product {
+
+}
